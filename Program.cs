4d8192bfae0be3f2a9ae4fa64aff045c2be8acc2
d11416146 <d11416146@gs.takming.edu.tw>
2026-06-24 14:39:55 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace finaltest_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IName n1;
+            string input = Console.ReadLine();
+            if (input == "t")
+            {
+                n1 = new Teacher();
+            }
+            else
+            {
+                n1 = new Student();
+            }
+            Console.WriteLine(n1.GetName());
+        }
+    }
+}
